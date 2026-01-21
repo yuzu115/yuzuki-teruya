74d774a9418ffcd2fc4a08040fe24cf543a5e738
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2026-01-21 15:13:23 +0900

    変更

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -5,10 +5,18 @@ int main() {
     int a = 20;
     int b = 5;
 
+    printf("=== l‘¥‰‰ZƒvƒƒOƒ‰ƒ€ ===\n");
+
     printf("‘«‚µZ: %d + %d = %d\n", a, b, add(a, b));
     printf("ˆø‚«Z: %d - %d = %d\n", a, b, sub(a, b));
     printf("Š|‚¯Z: %d * %d = %d\n", a, b, mul(a, b));
-    printf("Š„‚èZ: %d / %d = %d\n", a, b, divi(a, b));
+
+    if (b != 0) {
+        printf("Š„‚èZ: %d / %d = %d\n", a, b, divi(a, b));
+    }
+    else {
+        printf("Š„‚èZ: 0‚ÅŠ„‚é‚±‚Æ‚Í‚Å‚«‚Ü‚¹‚ñ\n");
+    }
 
     return 0;
 }
