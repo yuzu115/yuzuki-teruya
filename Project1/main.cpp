3a61b88b54e418b43d80d7de0fce14de86e38913
yuzu115 <120348154+yuzu115@users.noreply.github.com>
2026-01-21 15:19:23 +0900

    変えた

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,14 +1,18 @@
-#include <stdio.h>
-#include "calc.h"
+#include <iostream>
+using namespace std;
+
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int divi(int a, int b);
 
 int main() {
-    int a = 20;
-    int b = 5;
+    int a = 20, b = 5;
 
-    printf("‘«‚µŽZ: %d + %d = %d\n", a, b, add(a, b));
-    printf("ˆø‚«ŽZ: %d - %d = %d\n", a, b, sub(a, b));
-    printf("Š|‚¯ŽZ: %d * %d = %d\n", a, b, mul(a, b));
-    printf("Š„‚èŽZ: %d / %d = %d\n", a, b, divi(a, b));
+    cout << "‘«‚µŽZ: " << a << " + " << b << " = " << add(a, b) << endl;
+    cout << "ˆø‚«ŽZ: " << a << " - " << b << " = " << sub(a, b) << endl;
+    cout << "Š|‚¯ŽZ: " << a << " * " << b << " = " << mul(a, b) << endl;
+    cout << "Š„‚èŽZ: " << a << " / " << b << " = " << divi(a, b) << endl;
 
     return 0;
 }
